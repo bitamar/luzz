5f8b275fb86da66df253c32523284eb9df8cffb3
bitamar@gmail.com <bitamar@gmail.com>
2025-08-31 20:45:35 +0300

    chore(ci,tooling): unify Node via .nvmrc, consolidate ESLint/Prettier, add Pages deploy; clean API ESLint and CI triggers

---
+++ prettier.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('prettier').Config} */
+export default {
+  printWidth: 100,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  plugins: ['prettier-plugin-svelte'],
+  overrides: [{ files: ['**/*.svelte'], options: { parser: 'svelte' } }],
+};
